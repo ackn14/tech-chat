a06d89b4b4ece25891021e9b7a7cca7e128d7dd0
吉崎和真 <f14vn@MacBook-Pro.local>
2020-11-23 17:50:06 +0900

    1123

---
+++ components/DeleteRoom.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <v-card-actions class="pt-0">
+      <v-col cols="2" md="2">
+        <v-btn icon color="grey" text dark @click="remove(task.id)">
+          <v-icon>mdi-close-circle-outline</v-icon>
+        </v-btn>
+      </v-col>
+    </v-card-actions>
+  </v-card>
+</template>
--- store/rooms.js
+++ store/rooms.js
@@ -1,68 +1,68 @@
 export const state = () => ({
   rooms: []
-})
+});
 
 export const getters = {
   rooms: state => state.rooms
-}
+};
 
 export const mutations = {
   add(state, { room }) {
-    const isNotAdded = !state.rooms.find(r => r.id === room.id)
+    const isNotAdded = !state.rooms.find(r => r.id === room.id);
 
     if (isNotAdded) {
-      state.rooms.push(room)
+      state.rooms.push(room);
     }
   },
 
   update(state, { room }) {
     state.rooms = state.rooms.map(r => {
       if (r.id === room.id) {
-        r = room
+        r = room;
       }
-      return r
-    })
+      return r;
+    });
   },
 
   remove(state, { room }) {
-    state.rooms = state.rooms.filter(r => r.id !== room.id)
+    state.rooms = state.rooms.filter(r => r.id !== room.id);
   },
 
   clear(state) {
-    state.rooms = []
+    state.rooms = [];
   }
-}
+};
 
 export const actions = {
   subscribe({ commit }) {
     return this.$firestore
-      .collection('rooms')
-      .orderBy('createdAt', 'asc')
+      .collection("rooms")
+      .orderBy("createdAt", "desc")
       .onSnapshot(roomsSnapShot => {
         roomsSnapShot.docChanges().forEach(snapshot => {
           const room = {
             id: snapshot.doc.id,
             ...snapshot.doc.data()
-          }
+          };
 
           switch (snapshot.type) {
-            case 'added':
-              commit('add', { room })
-              break
+            case "added":
+              commit("add", { room });
+              break;
 
-            case 'modified':
-              commit('update', { room })
-              break
+            case "modified":
+              commit("update", { room });
+              break;
 
-            case 'removed':
-              commit('remove', { room })
-              break
+            case "removed":
+              commit("remove", { room });
+              break;
           }
-        })
-      })
+        });
+      });
   },
 
   clear({ commit }) {
-    commit('clear')
+    commit("clear");
   }
-}
+};
